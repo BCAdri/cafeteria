e8c64c3fcada924872c18b215e5aa541317644b2
BCAdri <byadrian05@gmail.com>
2024-04-29 12:35:30 +0200

    Menu incial, home, routes.
    Creación de archivos principales, configuración de las rutas.

--- src/navigation/index.jsx
+++ src/navigation/index.jsx
@@ -1,10 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Header } from "../components/Header";
+import Home from "../pages/Home";
+import Cart from "../pages/Cart";
+import Login from "../pages/Login";
+import Menu from "../pages/Menu";
+import Register from "../pages/Register";
 
 const Navigation = () => {
     return (
         <BrowserRouter>
             <Header />
+            <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="/login" element={<Login />}/>
+                <Route path="/register" element={<Register />}/>
+                <Route path="/menu" element={<Menu />}/>
+                <Route path="/cart" element={<Cart />}/>
+
+            </Routes>
         </BrowserRouter>
     )
 }
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,7 @@
+const Home = () =>{
+    return (
+        <div className="text-withe">Home</div>
+    )
+}
+
+export default Home;
